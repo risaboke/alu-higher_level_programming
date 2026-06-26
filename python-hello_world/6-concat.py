4015478b7f06a56187cb5c15f63e9f7563c26574
Rebecca <r.isaboke@alustudent.com>
2026-06-26 05:15:13 -0700

    Fix task 6 to use str1 and str2

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str1 = "Programming is like building a multilingual puzzle"
-str2 = "Holberton School"
-print("Welcome to " + str2 + "!")
+str1 = "Welcome to "
+str2 = "Holberton School!"
+print(str1 + str2)
