4acee43a3148e421747c6964156895f2bc22dc76
Rebecca <r.isaboke@alustudent.com>
2026-06-26 14:39:51 -0700

    Fix 3-print_alphabt.py task

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for i in range(26):
-    if chr(ord('a') + i) != 'q' and chr(ord('a') + i) != 'e':
-        print("{:c}".format(ord('a') + i), end="")
-print("")
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
