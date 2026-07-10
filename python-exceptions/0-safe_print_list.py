d103072cd0adfeb8b2b146ee545073494803efc0
risaboke <r.isaboke@alustudent.com>
2026-07-11 00:02:52 +0200

    Fix indentation in safe_print_list function

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
     for i in range(x):
@@ -8,4 +7,4 @@ def safe_print_list(my_list=[], x=0):
         except IndexError:
             break
     print()
-    return count 
+    return count
